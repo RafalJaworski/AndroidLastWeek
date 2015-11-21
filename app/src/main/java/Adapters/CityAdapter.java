f5fdf16ccce00fa91e1e3ba6a50fc0ddd9058292
RafalJaworski <admin@lux-price.pl>
2015-11-21 12:26:15 +0100

    CityViewHolder extends by Cityadapter

--- app/src/main/java/Adapters/CityAdapter.java
+++ app/src/main/java/Adapters/CityAdapter.java
@@ -3,10 +3,8 @@ package Adapters;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
-/**
- * Created by jawa on 21/11/2015.
- */
-public class CityAdapter {
+
+public class CityAdapter extends RecyclerView.Adapter<CityAdapter.CityViewHolder>{
 
     public static class CityViewHolder extends RecyclerView.ViewHolder {
         public CityViewHolder(View itemView) {
