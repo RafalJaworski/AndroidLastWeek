1309739376a8c7e8bd2a6efcf7e719790f505dc2
RafalJaworski <admin@lux-price.pl>
2015-11-21 12:24:52 +0100

    City ViewHolder created

--- app/src/main/java/Adapters/CityAdapter.java
+++ app/src/main/java/Adapters/CityAdapter.java
@@ -1,7 +1,16 @@
 package Adapters;
 
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
 /**
  * Created by jawa on 21/11/2015.
  */
 public class CityAdapter {
+
+    public static class CityViewHolder extends RecyclerView.ViewHolder {
+        public CityViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
 }
