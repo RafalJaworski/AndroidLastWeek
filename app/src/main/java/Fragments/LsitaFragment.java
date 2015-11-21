c4adbc60c4112d47a86dce8b80627dda0de7081a
RafalJaworski <admin@lux-price.pl>
2015-11-21 11:46:32 +0100

    onCreateView changed

--- app/src/main/java/Fragments/LsitaFragment.java
+++ app/src/main/java/Fragments/LsitaFragment.java
@@ -7,11 +7,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import domenafirmy.lastweek.R;
+
 public class LsitaFragment extends Fragment{ //fragment z support.v4
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+        return inflater.inflate(R.layout.fragment_lista,container,false);
     }
 }
