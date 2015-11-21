ec2447539eb5149f5d9f32908d86ae99c3444434
RafalJaworski <admin@lux-price.pl>
2015-11-21 11:51:13 +0100

    doscrition comment added

--- app/src/main/java/Fragments/LsitaFragment.java
+++ app/src/main/java/Fragments/LsitaFragment.java
@@ -13,7 +13,12 @@ public class LsitaFragment extends Fragment{ //fragment z support.v4
 
     @Nullable
     @Override
+    //zwracamy
+    //inflater tworzy z xmla
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        //container - tu moze zostac zwrocone jak ostatni parametr bylby true
+        // ale piszemy zawsze piszemy FALSE w metodzie ONCREATEVIEW
+        //attach to Root = false - zwraca ale nie todaje do konttenera
         return inflater.inflate(R.layout.fragment_lista,container,false);
     }
 }
