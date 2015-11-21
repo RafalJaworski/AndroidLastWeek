56e15c5a0090401c19f8538d66fc2c9542537a55
RafalJaworski <admin@lux-price.pl>
2015-11-21 11:44:13 +0100

    overdide default onCreateView

--- app/src/main/java/Fragments/LsitaFragment.java
+++ app/src/main/java/Fragments/LsitaFragment.java
@@ -1,7 +1,17 @@
 package Fragments;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class LsitaFragment extends Fragment{ //fragment z support.v4
 
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
 }
