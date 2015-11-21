d0671ce4f07502d61ae236e36f2c5e325c72dab7
RafalJaworski <admin@lux-price.pl>
2015-11-21 13:22:16 +0100

    inflate view

--- app/src/main/java/domenafirmy/lastweek/Fragments/SzczegolyFragment.java
+++ app/src/main/java/domenafirmy/lastweek/Fragments/SzczegolyFragment.java
@@ -1,7 +1,19 @@
 package domenafirmy.lastweek.Fragments;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import domenafirmy.lastweek.R;
 
 
 public class SzczegolyFragment extends Fragment{
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_szczegoly,container,false);
+    }
 }
