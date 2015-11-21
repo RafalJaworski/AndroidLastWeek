0f9d62216c374674b20021184380dae87599fc9a
RafalJaworski <admin@lux-price.pl>
2015-11-21 12:29:56 +0100

    neccessary methods implemented

--- app/src/main/java/Adapters/CityAdapter.java
+++ app/src/main/java/Adapters/CityAdapter.java
@@ -2,9 +2,27 @@ package Adapters;
 
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
 
 
 public class CityAdapter extends RecyclerView.Adapter<CityAdapter.CityViewHolder>{
+
+    @Override
+    public CityViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+
+    @Override
+    public void onBindViewHolder(CityViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
 
     public static class CityViewHolder extends RecyclerView.ViewHolder {
         public CityViewHolder(View itemView) {
