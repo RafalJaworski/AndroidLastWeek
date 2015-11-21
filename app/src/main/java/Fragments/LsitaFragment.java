5922a4ffc1817ff297c4ba0a72877ebdfc941c19
RafalJaworski <admin@lux-price.pl>
2015-11-21 11:53:26 +0100

    new RecyclerView variable

--- app/src/main/java/Fragments/LsitaFragment.java
+++ app/src/main/java/Fragments/LsitaFragment.java
@@ -3,13 +3,18 @@ package Fragments;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import butterknife.Bind;
 import domenafirmy.lastweek.R;
 
 public class LsitaFragment extends Fragment{ //fragment z support.v4
+
+    @Bind(R.id.lista)
+    protected RecyclerView lista;
 
     @Nullable
     @Override
