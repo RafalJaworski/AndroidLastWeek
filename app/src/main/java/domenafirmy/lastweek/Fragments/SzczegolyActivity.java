a387895e235512b57f708f8554f5ad690857c164
RafalJaworski <admin@lux-price.pl>
2015-11-21 13:46:49 +0100

    new Activity/EmptyActivity

---
+++ app/src/main/java/domenafirmy/lastweek/Fragments/SzczegolyActivity.java
@@ -0,0 +1,15 @@
+package domenafirmy.lastweek.Fragments;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import domenafirmy.lastweek.R;
+
+public class SzczegolyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_szczegoly);
+    }
+}
