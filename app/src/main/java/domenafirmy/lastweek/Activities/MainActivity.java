aa7f04d349ee8962242dde4e5dbbca2b610e9041
RafalJaworski <admin@lux-price.pl>
2015-11-21 16:45:58 +0100

    OnItemClick extends by MainActivity

--- app/src/main/java/domenafirmy/lastweek/Activities/MainActivity.java
+++ app/src/main/java/domenafirmy/lastweek/Activities/MainActivity.java
@@ -3,9 +3,11 @@ package domenafirmy.lastweek.Activities;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import domenafirmy.lastweek.Adapters.CityAdapter;
 import domenafirmy.lastweek.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+    implements CityAdapter.OnItemClick{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
