a967566593fd7bfcefddbb7c8fbaa7f0c9e5fb3d
RafalJaworski <admin@lux-price.pl>
2015-11-21 12:40:10 +0100

    inflater added

--- app/src/main/java/Adapters/CityAdapter.java
+++ app/src/main/java/Adapters/CityAdapter.java
@@ -1,6 +1,8 @@
 package Adapters;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -11,9 +13,12 @@ import java.util.List;
 public class CityAdapter extends RecyclerView.Adapter<CityAdapter.CityViewHolder>{
 
     private List<String> dane;
+    private LayoutInflater inflater;
 
-    public CityAdapter(List<String> dane) {
+    //musimy dodac context bo inflater bierze sie z contextu
+    public CityAdapter(Context context, List<String> dane) {
         this.dane = dane;
+        this.inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
     }
 
     @Override
